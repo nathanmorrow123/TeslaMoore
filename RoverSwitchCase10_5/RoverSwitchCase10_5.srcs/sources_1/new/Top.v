2dc02b773270a224802da49caac73ecb29838a5e
matthewsoon8 <91095496+matthewsoon8@users.noreply.github.com>
2021-10-05 16:00:59 -0500

    New Program
    Added A new program named by today's date

---
+++ RoverSwitchCase10_5/RoverSwitchCase10_5.srcs/sources_1/new/Top.v
@@ -0,0 +1,85 @@
+`timescale 1ns / 1ps
+
+
+module Top(
+
+    input CLK100MHZ, reset,
+    input [15:0] sw,  //the 4 inputs for each display
+    input outerLeft,
+    //input innerLeft,
+    //input innerRight,
+    input outerRight,
+    input Sensor,
+    //output [6:0] JC,   //output to H-Bridge
+    output enableA, directionPinAF, directionPinAB,
+    output enableB,directionPinBF,directionPinBB,
+    
+    output a, b, c, d, e, f, g, dp, //the individual LED output for the seven segment along with the digital point
+    output [3:0] an,   // Annodes from the constriants file
+    output [15:0] LED
+ );
+
+reg [20:0] maxValue = 200000;   //Max counter value
+reg [18:0] lowSpeed = 90000;
+
+reg [18:0] counter =0;        //Counter for this project
+
+reg [18:0] rightPWM;
+reg [18:0] rightDirection;
+reg [18:0] leftPWM;
+reg [18:0] leftDirection;
+
+reg [18:0] slowPwm;
+reg [18:0] fastPwm;
+
+
+always@(posedge CLK100MHZ)begin //clock
+    if (counter <maxValue) //count until 100
+        counter <=counter+1; 
+    else
+        counter <=0; //reset counter              
+end      
+/*
+always@(posedge CLK100MHZ)begin 
+    switch
+              
+end  
+*/
+always@(posedge CLK100MHZ)begin //clock
+   if(outerLeft == 0 )begin
+        leftPWM <= 200000;
+        leftDirection <= 0;
+        rightDirection <= 1;
+   end else begin
+       leftDirection <= 1;
+        rightDirection <= 1;
+   end
+   
+   if(outerRight == 0)begin
+        rightPWM <= 200000;
+        leftDirection <= 1;
+        rightDirection <= 0;
+    end else begin
+       leftDirection=1;
+        rightDirection=1;
+   end
+                 
+end     
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////
+    //left      enable A        Left
+    assign enableA = (Sensor && counter < lowSpeed) ? 1:0;      //JC[4] enableA
+    not leftLed (LED[14], outerLeft );                          //enableA
+    assign directionPinAF= (~leftDirection) ? 1:0;                 //JC[6] forward
+    assign directionPinAB= (leftDirection) ? 1:0;                  //JC[5] Backward
+    
+    
+    //right     enable B        right
+    assign enableB = ( Sensor && counter < lowSpeed) ? 1:0;     //JC[0]
+    not rightLed (LED[1], outerRight );                         //enableB     
+    assign directionPinBF= (~rightDirection) ? 1:0;                //JC[1]   Forward
+    assign directionPinBB= (rightDirection) ? 1:0;                 // JC[2]   Backward
+    
+    
+   
+endmodule
